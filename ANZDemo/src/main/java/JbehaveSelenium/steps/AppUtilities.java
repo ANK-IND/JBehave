53ce575d5221e89a65a89f453a287ca00020e7c1
nirmalkumar <nirmalkumar@192.168.1.3>
2017-04-12 14:48:47 +0530

    Changes for Jenkins

--- ANZDemo/src/main/java/JbehaveSelenium/steps/AppUtilities.java
+++ ANZDemo/src/main/java/JbehaveSelenium/steps/AppUtilities.java
@@ -9,7 +9,7 @@ public class AppUtilities {
 	public static Properties prop = new Properties();
 	
 	static {
-		try(FileInputStream fis = new FileInputStream(new File("./Config.properties"))) {
+		try(FileInputStream fis = new FileInputStream(new File("/Users/nirmalkumar/Documents/Git_Repo/JBehave/ANZDemo/Config.properties"))) {
 			prop.load(fis);	
 		} catch (Exception e) {
 			e.printStackTrace();
